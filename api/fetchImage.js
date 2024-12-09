8ca3e841321a403791bd8782580714ff5cd1bc69
Tanmay Chaturvedi <168345467+tanmayc43@users.noreply.github.com>
2024-12-09 19:47:52 +0000

    updated fetchImage.js

--- api/fetchImage.js
+++ api/fetchImage.js
@@ -1,14 +1,18 @@
 const fetch = require('node-fetch');
 
 export default async function handler(req, res) {
-  const apiKey = process.env.UNSPLASH_API_KEY;
+  const apiKey = process.env.UNSPLASH_API_KEY; 
   const apiUrl = `https://api.unsplash.com/photos/random?client_id=${apiKey}&orientation=landscape&query=nature`;
 
   try {
+    console.log('Fetching image from Unsplash...');
     const response = await fetch(apiUrl);
+    
     if (!response.ok) {
+      console.error('Error fetching image:', response.status, response.statusText);
       return res.status(response.status).json({ error: 'Error fetching image' });
     }
+    
     const data = await response.json();
     return res.status(200).json({ imageUrl: data.urls.full });
   } catch (error) {
